ce2a642ac0b6e62ed8150632235ad6851f1a3e30
Apartkktrain <64818647+Apartkktrain@users.noreply.github.com>
2020-10-04 11:10:07 +0900

    if文をcase文に変更&きれいに

--- src/launchpencil/joinmessage/Main.php
+++ src/launchpencil/joinmessage/Main.php
@@ -37,17 +37,19 @@ class main extends pluginBase implements Listener
         $quit3 = "§a$name さんがサーバーのエラーにより退出しました。";
 
         //退出時のif判定
-        if ($reason === 'client disconnect') {
+        switch($reason)
+        {
+            case 'client disconnect':
                 $event->setQuitMessage($quit1);//ここで上のものを流す
-                return true;
-        }
-        if ($reason === 'timeout') {
+                break;
+                
+            case 'timeout':
                 $event->setQuitMessage($quit2);//ここで上のものを流す
-                return true;
-        }
-        if ($reason === 'Internal server error') {
-                $event->setQuitMessage($quit3);//ここで上のものを流す
-                return true;
-        }
-        }
+                break;
+                
+            case 'Internal server error':
+                $event->setQuitMessage($quit3);
+                break;
+        }       
+     }
 }
